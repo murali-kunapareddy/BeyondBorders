83d6eb8c930c4b5b8ad14a46a5277f7992a57e72
Murali Krishna Kunapareddy <murali.kunapareddy@outlook.com>
2024-02-24 20:27:27 +0530

    Adding BB.Web

---
+++ BB.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BB.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
